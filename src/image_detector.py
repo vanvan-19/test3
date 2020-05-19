88712b0f05c87736dd658b29986ba1462a47abfe
Felix Husen <felixhusen@gmail.com>
2020-05-19 11:29:48 +1000

    Implement image labelling

--- src/image_detector.py
+++ src/image_detector.py
@@ -6,14 +6,19 @@ class ImageDetector():
     GOOGLE_VISION_API_URL = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCAWdPG_HuzbBTTHM4MsVOv82qIIzeAC34"
 
     '''
-    #1 This method initialise the class with the parameter of image url
+    #1 This function initialises the class with the parameter of image url
     '''
     def __init__(self, image_url):
         # Initialise Google Vision API
         self.image_url = image_url
 
     '''
-    #2 This method will analyse and label the image
+    #2 This function will analyse and label the image
+
+    Iteration 0:
+    def get_image_labels(self, max_results = 10):
+        pass
+
     '''
     def get_image_labels(self, max_results = 10):
         # Trigger the google vision api to generate labelling
@@ -37,5 +42,16 @@ class ImageDetector():
         }
 
         result = requests.post(url=self.GOOGLE_VISION_API_URL, json=data)
-        return json.loads(result.text)["responses"][0]["labelAnnotations"]
-        
+        result = json.loads(result.text)["responses"][0]["labelAnnotations"]
+        result = [label["description"] for label in result]
+        return result
+    
+    '''
+    #3 Create a function that can detect object from an image. The function will return a list of detected object from the google vision API
+       Note that first test must fail. Hint: def get_image_objects(self, ...): pass
+    '''
+
+    '''
+    #4 Create a function that can detect face from an image. The function will return the list of faces detected within an image.
+       Note that first test must fail. Hint: def get_image_faces(self, ...): pass
+    '''
--- test/test_image_detector.py
+++ test/test_image_detector.py
@@ -4,25 +4,44 @@ sys.path.append('src/')
 from image_detector import ImageDetector
 import unittest
 
+'''
+Test Driven Development (TDD) says when developing a software/application, we develop a software with the expected test result. Therefore, the next part of the software would not continue before the test has passed.
+
+'''
+
 class TestImageDetector(unittest.TestCase):
+
+    # This is the test cases.
 
     BIRTHDAY_IMAGE_URL = "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/960/330/90/media/dm/en-us/global/social-events/event-types/SocialEvents-TypesofEvents-Hero-5x2..jpg"
 
     '''
-    #1 This method sets up the image detector object.
+    #1 This function sets up the image detector object.
     '''
     @classmethod
     def setUpClass(self):
         self.image_detector = ImageDetector(image_url=self.BIRTHDAY_IMAGE_URL)
 
     '''
-    #2 This method test the image labelling
+    #2 This function tests the image labelling
+
+    TODO: Create other test cases for each of the function
+
+    Iteration 0: FAIL, because the returned value of get_image_labels() contains nothing.
+
     '''
     def test_get_image_labels(self):
         expected_labels = ['Event', 'Ceremony', 'Alcohol', 'Happy', 'Drink', 'Leisure', 'Party', 'Fashion accessory', 'Conversation', 'Taste']
         labels = self.image_detector.get_image_labels()
-        labels = [label["description"] for label in labels]
         self.assertListEqual(expected_labels, labels)
+
+    '''
+    #3 Create a test function that can detect object from an image. Look at the test above.
+    '''
+
+    '''
+    #3 Create a test function that can detect object from an image. Look at the test above.
+    '''
 
 if __name__ == "__main__":
     unittest.main()
